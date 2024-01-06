270fcc71141a0302dfd044cefff1f5c1e8a3666c
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2024-01-06 00:45:51 -0800

    update commit

---
+++ singlePage/singlepage.js
@@ -0,0 +1,23 @@
+function showPage(page) {
+
+    // Hide all of the divs:
+    document.querySelectorAll('div').forEach(div => {
+        div.style.display = 'none';
+    });
+
+    // Show the div provided in the argument
+    document.querySelector(`#${page}`).style.display = 'block';
+}
+
+// Wait for page to loaded:
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Select all buttons
+    document.querySelectorAll('button').forEach(button => {
+
+        // When a button is clicked, switch to that page
+        button.onclick = function() {
+            showPage(this.dataset.page);
+        }
+    })
+});
