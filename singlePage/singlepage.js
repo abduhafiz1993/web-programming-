a4f7288e6c2ea78cd71415d56c9d0ad99807d93a
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2024-01-06 00:50:33 -0800

    adding index.html

--- singlePage/singlepage.js
+++ singlePage/singlepage.js
@@ -1,23 +1,20 @@
-function showPage(page) {
-
-    // Hide all of the divs:
+function showpage(page){
     document.querySelectorAll('div').forEach(div => {
         div.style.display = 'none';
     });
+    document.querySelector(`#${page}`).style.display ='block';
 
-    // Show the div provided in the argument
-    document.querySelector(`#${page}`).style.display = 'block';
 }
 
-// Wait for page to loaded:
-document.addEventListener('DOMContentLoaded', function() {
 
-    // Select all buttons
+document.addEventListener('DOMContentLoaded', function(){
+
     document.querySelectorAll('button').forEach(button => {
-
-        // When a button is clicked, switch to that page
-        button.onclick = function() {
-            showPage(this.dataset.page);
+            button.onclick = function() {
+                showpage(this.dataset.page)
         }
-    })
+    }
+    )
+
+
 });
